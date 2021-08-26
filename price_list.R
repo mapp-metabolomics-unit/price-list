ce7f4ca17317985f9073a3067f8de8a53523b286
PMA <pierre-marie.allard@unifr.ch>
2021-08-26 11:46:51 +0200

    price list calced

--- price_list.R
+++ price_list.R
@@ -6,10 +6,16 @@ library(tidyr)
 library(tidyverse)
 
 
+# Ressources
+# https://www.statforbiology.com/2020/stat_nls_usefulfunctions/
+
+
+
+
 
 
 samples <- c(0,1, 2, 5,10,20,50,75,100,250,500, 1000)
-tarifs <- c(0,100,120,150,200,300,450,600, 700,1000, 1250, 1500)
+tarifs <- c(0,40,100,150,200,300,450,600, 700,1000, 1250, 1500)
 mat <- data.frame(samples,tarifs)
 
 model <- drm(tarifs ~ samples, fct = DRC.asymReg(), data = mat)
@@ -30,59 +36,20 @@ input_df = data.frame(samples, tarifs)
 
 merged_df = merge(input_df, mat_pred, by = 'samples', all.y = TRUE)
 
-
-
-fig <- plot_ly(x = samples, y = tarifs)
-fig <- add_trace(data = mat_pred, x = mat_pred$samples, y = mat_pred$pred)
-fig
-
+merged_df$pred_rounded = round(merged_df$pred, digits = -1)
 
 fig <- plot_ly(data = merged_df, x = ~samples, y = ~tarifs, type = 'scatter') %>% 
-        add_trace(x =~samples,  y = ~pred , type = 'scatter', mode = 'lines+markers')
+        add_trace(x =~samples,  y = ~pred_rounded , type = 'scatter', mode = 'lines+markers')
 fig
 
-pred[1]
+
+# selection 
+
+merged_df_sel = merged_df  %>% 
+mutate(prix_CHF = pred_rounded)  %>% 
+select(samples, prix_CHF)
+
+# output
 
 
-library(plotly)
-data <- data.frame(
-  X = c (0, 1, 2, 3, 4, 5),
-  Line = c(1.5, 1, 1.3, 0.7, 0.8, 0.9),
-  Bar = c(1, 0.5, 0.7, -1.2, 0.3, 0.4))
-
-fig <- plot_ly(data , x = ~X, y = ~Bar, type = 'bar') %>%
-  add_trace(data , x = ~X, y = ~Line, type = 'scatter',  mode = 'lines+markers')
-
-fig
-
-# on cherche a faire des paliers dans la putain de courbe asymptot
-
-paliers = as.numeric(cut(pred, breaks = 10))
-
-
-classes = unique(cut(mat_pred$samples, breaks = 10))
-
-
-values = round(tapply(pred,paliers, min))
-
-names(values) = classes
-
-
-# on arrete la et il manque a faire la table propre 
-
-pred_df = as.data.frame(pred)
-
-pred_df <- tibble::rownames_to_column(pred_df, "Samples_number")
-
-pred_df$Samples_number = as.numeric(pred_df$Samples_number)
-
-hist(pred_df$Samples_number)
-hist(pred_df$pred, breaks="Sturges")
-
-plot(pred_df)
-
-
-library(Hmisc) # cut2
-split(pred_df, cut2(pred_df$pred))
-
-pred_df$gp <- split(pred_df, cut2(pred_df$pred, 10))
+write_csv(merged_df_sel, 'data/price_bioinf.csv')
