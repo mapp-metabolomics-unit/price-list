c1e11308b2151fdbe7986153c6b3df559d58c47c
PMA <pierre-marie.allard@unifr.ch>
2021-08-26 20:09:18 +0200

    update with recode verb

--- price_app/app.R
+++ price_app/app.R
@@ -1,10 +1,11 @@
-
 library(shiny)
 library(ggplot2)
 library(dplyr)
 
-price_menu <- read.csv("./data/price_menu.csv", stringsAsFactors = FALSE)
-price_bioinfo <- read.csv("./data/price_bioinf.csv", stringsAsFactors = FALSE)
+
+price_menu <- read.csv("price_app/data/price_menu.csv", stringsAsFactors = FALSE)
+price_bioinfo <- read.csv("price_app/data/price_bioinf.csv", stringsAsFactors = FALSE)
+
 
 print(str(price_menu))
 
@@ -15,31 +16,37 @@ ui <- fluidPage(
     sidebarPanel(
       sliderInput("sampleInput", "Samples", 0, 500, 5, pre = ""),
       radioButtons("typeInput", "User",
-                  choices = c("unifr", "academics", "commercials"),
-                  selected = "unifr"),
+        choices = c(
+          "UniFr" = "unifr",
+          "Academics" = "academics",
+          "Private" = "private"
+        ),
+        selected = "unifr"
+      ),
       selectInput("spl_itemInput", "Sample Prep",
-              c('None', sort(unique(price_menu$item[grep('spl_prep', price_menu$item)]))) ,
-              selected = "None"),
+        c("None", sort(unique(price_menu$Item[grep("Vial|Prep", price_menu$Item)]))),
+        selected = "None"
+      ),
       selectInput("da_itemInput", "Data Acquisition",
-              c('None', sort(unique(price_menu$item[grep('da', price_menu$item)]))) ,
-              selected = "None"),
+        c("None", sort(unique(price_menu$Item[grep("da", price_menu$Item)]))),
+        selected = "None"
+      ),
       selectInput("proc_itemInput", "Processing",
-              c('None', sort(unique(price_menu$item[grep('processing', price_menu$item)]))) ,
-              selected = "None"),
+        c("None", sort(unique(price_menu$Item[grep("processing", price_menu$Item)]))),
+        selected = "None"
+      ),
       checkboxInput("bioInput_biostats", "Biostats", FALSE),
       checkboxInput("bioInput_metannot", "Metabolite Annotation", FALSE),
-    #   selectInput("itemInput", "Item",
-    #           sort(unique(price_menu$item)),
-    #           selected = "None"),
+      #   selectInput("itemInput", "Item",
+      #           sort(unique(price_menu$Item)),
+      #           selected = "None"),
       uiOutput("countryOutput")
     ),
     mainPanel(
       textOutput("coolplot"),
-      tags$head(tags$style("#coolplot{color: black;
-                                 font-size: 200px;
+      tags$head(tags$style("#coolplot{color: black; font-size: 200px;
                                  font-style: bold;
-                                 }")
-      ),
+                                 }")),
       br(), br(),
       tableOutput("results")
     )
@@ -47,29 +54,28 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
-#   output$countryOutput <- renderUI({
-#     selectInput("countryInput", "Country",
-#                 sort(unique(bcl$Country)),
-#                 selected = "CANADA")
-#   })  
-  
+  #   output$countryOutput <- renderUI({
+  #     selectInput("countryInput", "Country",
+  #                 sort(unique(bcl$Country)),
+  #                 selected = "CANADA")
+  #   })
+
   filtered <- reactive({
     if (is.null(input$typeInput)) {
       return(NULL)
-    }    
-      price_menu %>%
-      select(item, input$typeInput)  %>% 
-      filter(item == input$spl_itemInput | item == input$da_itemInput | item == input$proc_itemInput )
+    }
+    price_menu %>%
+      select(Item, input$typeInput) %>%
+      filter(Item == input$spl_itemInput | Item == input$da_itemInput | Item == input$proc_itemInput)
   })
 
-    filtered_2 <- reactive({
+  filtered_2 <- reactive({
     if (is.null(input$typeInput)) {
       return(NULL)
-    }    
-      price_bioinfo %>%
-      filter(samples == input$sampleInput[1])  %>% 
-      select('prix_CHF')
-
+    }
+    price_bioinfo %>%
+      filter(samples == input$sampleInput[1]) %>%
+      select("prix_CHF")
   })
 
 
@@ -77,11 +83,16 @@ server <- function(input, output) {
     if (is.null(filtered())) {
       return()
     }
-    paste((sum(filtered()[,2] * input$sampleInput[1]) + (filtered_2() * (as.numeric(input$bioInput_biostats) + as.numeric(input$bioInput_metannot)))), ' CHF')
+    paste((sum(filtered()[, 2] * input$sampleInput[1]) + (filtered_2() * (as.numeric(input$bioInput_biostats) + as.numeric(input$bioInput_metannot)))), " CHF")
   })
 
   output$results <- renderTable({
-    filtered()
+    filtered() %>%
+      rename_with(recode,
+        unifr = "UniFr",
+        academics = "Academics",
+        private = "Private"
+      )
   })
 }
 
